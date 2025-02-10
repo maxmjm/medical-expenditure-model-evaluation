4049f1a66c59207669dfd3bcd9bf381747c05913
Maxime Morard <maxmjm@icloud.com>
2025-02-10 11:20:14 +0800

    Renamed variable

--- model.py
+++ model.py
@@ -4,5 +4,5 @@ import pandas
 df = pandas.read_csv("medical_charges_with_descriptors.csv")
 
 # Examine file structure
-file_structure = df.info()
-print(file_structure)
+df_structure = df.info()
+print(df_structure)
