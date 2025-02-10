2ec1be8c7b6822c3febc93880eaf4081cd428be4
Maxime Morard <maxmjm@icloud.com>
2025-02-10 11:33:43 +0800

    Describe data file contents and print to console

--- model.py
+++ model.py
@@ -6,3 +6,7 @@ df = pandas.read_csv("medical_charges_with_descriptors.csv")
 # Examine file structure
 df_structure = df.info()
 print(df_structure)
+
+# Describe file contents
+df_describe = df.describe()
+print(df_describe)
